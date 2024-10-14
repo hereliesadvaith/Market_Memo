52cb64a791eef336b3dae0719f9cc0e28d0ad98d
Advaith B G <hereliesadvaith@gmail.com>
2024-10-14 23:20:29 +0530

    [ADD] frontend: orm call function

--- frontend/static/root.js
+++ frontend/static/root.js
@@ -1,13 +1,29 @@
-import { Component, mount, xml } from "@odoo/owl"
-
+import { Component, onWillStart, mount, xml } from "@odoo/owl";
 
 class Root extends Component {
   setup() {
-    console.log('hii')
+    onWillStart(async () => {
+      var result = await this.makeApiCall("/api/orm/strategy/1/", "GET");
+      console.log(result);
+    });
   }
+
+  async makeApiCall(url, method, data = null) {
+    var url = "http://localhost:8000" + url;
+    const config = {
+      method: method,
+    };
+    if (data && (method === "POST" || method === "PUT")) {
+      config.body = JSON.stringify(data);
+    }
+    const response = await fetch(url, config);
+    const result = await response.json()
+    return result;
+  }
+
   static template = xml`
     <h1>Vite + OWL</h1>
 `;
 }
 
-mount(Root, document.getElementById('root'))
+mount(Root, document.getElementById("root"));
